7d5271425703814adc6e6829601a8d723cda965b
JoelissonSS <113321562+JoelissonSS@users.noreply.github.com>
2022-11-19 01:38:37 -0300

    uptade 18/11
    resolvi a questao do display grid nao estar funcionando e centralizei a div Resumo

--- Portfólio/script.js
+++
@@ -1,5 +0,0 @@
-function mudar(atual){
-    let parag = document.getElementById(atual)
-    document.querySelectorAll('.pa').forEach(el => el.style.display = 'none')
-    parag.style.cssText = 'display: block;'
-}
